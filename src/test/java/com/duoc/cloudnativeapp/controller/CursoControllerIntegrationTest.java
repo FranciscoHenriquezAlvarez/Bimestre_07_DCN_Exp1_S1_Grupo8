5e8576c445e5ab9f8395db6f1ba5c667a3d6c8b9
Francisco Henriquez <fran.henriqueza@duocuc.cl>
2026-05-26 00:54:32 -0400

    Actualizacion 01 - Entrega Semana 1 Desarrollo Cloud Native

--- src/test/java/com/duoc/cloudnativeapp/controller/CursoControllerIntegrationTest.java
+++ src/test/java/com/duoc/cloudnativeapp/controller/CursoControllerIntegrationTest.java
@@ -5,15 +5,15 @@ import com.duoc.cloudnativeapp.repository.CursoRepository;
 import com.duoc.cloudnativeapp.repository.DetalleInscripcionRepository;
 import com.duoc.cloudnativeapp.repository.EstudianteRepository;
 import com.duoc.cloudnativeapp.repository.InscripcionRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 
